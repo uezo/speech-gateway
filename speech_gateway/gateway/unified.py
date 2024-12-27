09193585197acd62ece282312ab600d26545952a
uezo <uezo@uezo.net>
2024-12-27 18:00:17 +0900

    Add support for switching multiple languages🌏
    - Implemented language-based gateway selection for TTS services.
    - Added `languages` parameter to `add_gateway` for associating gateways with specific languages.
    - Introduced `default_speaker` option to simplify speaker selection for each request.
    
    Related issue #4

--- speech_gateway/gateway/unified.py
+++ speech_gateway/gateway/unified.py
@@ -1,25 +1,36 @@
-from typing import Dict
+from typing import Dict, List
 from fastapi import HTTPException
 from fastapi import Request, APIRouter
 from . import SpeechGateway, UnifiedTTSRequest
 
 
 class UnifiedGateway(SpeechGateway):
-    def __init__(self, *, default_gateway: SpeechGateway = None, debug = False):
+    def __init__(self, *, default_gateway: SpeechGateway = None, default_language: str = "ja-JP", debug = False):
         super().__init__(stream_source=None, debug=debug)
         self.service_map: Dict[str, SpeechGateway] = {}
+        self.language_map: Dict[str, SpeechGateway] = {}
+        self.default_speakers: Dict[SpeechGateway, str] = {}
         self.default_gateway: SpeechGateway = default_gateway
+        self.default_language = default_language
 
-    def add_gateway(self, service_name: str, gateway: SpeechGateway, default: bool = False):
+    def add_gateway(self, service_name: str, gateway: SpeechGateway, *, languages: List[str] = None, default_speaker: str = None, default: bool = False):
         self.service_map[service_name] = gateway
+        if languages:
+            for lang in languages:
+                self.language_map[lang] = gateway
         if default:
             self.default_gateway = gateway
+            self.language_map[self.default_language] = gateway
+        self.default_speakers[gateway] = default_speaker
 
-    def get_gateway(self, service_name: str = None) -> SpeechGateway:
-        if service_name:
-            return self.service_map.get(service_name)
+    def get_gateway(self, tts_request: UnifiedTTSRequest):
+        if tts_request.service_name:
+            return self.service_map.get(tts_request.service_name)
+        elif tts_request.language:
+            return self.language_map.get(tts_request.language)
         elif self.default_gateway:
             return self.default_gateway
+        return None
 
     def get_router(self) -> APIRouter:
         router = APIRouter()
@@ -29,10 +40,13 @@ class UnifiedGateway(SpeechGateway):
     def register_endpoint(self, router: APIRouter):
         @router.post("/tts")
         async def post_tts(request: Request, tts_request: UnifiedTTSRequest, x_audio_format: str = "wav"):
-            gateway = self.get_gateway(tts_request.service_name)
+            gateway = self.get_gateway(tts_request)
 
             if not gateway:
                 raise HTTPException(status_code=404, detail="No gateway found.")
+            
+            if not tts_request.speaker:
+                tts_request.speaker = self.default_speakers.get(gateway)
 
             return await gateway.unified_tts_handler(request, tts_request, x_audio_format)
 
