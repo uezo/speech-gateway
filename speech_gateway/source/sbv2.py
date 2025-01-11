7962f1ef69104385e455c12b8290e178f0799ecd
uezo <uezo@uezo.net>
2025-01-11 22:19:08 +0900

    Set correct format in cache key

--- speech_gateway/source/sbv2.py
+++ speech_gateway/source/sbv2.py
@@ -3,7 +3,7 @@ from . import StreamSource
 
 class StyleBertVits2StreamSource(StreamSource):
     def get_cache_key(self, audio_format: str, query_params: dict, **kwargs) -> str:
-        return f"{hash(str(query_params))}.{'mp3' if audio_format == 'mp3' else 'wav'}"
+        return f"{hash(str(query_params))}.{audio_format or 'wav'}"
 
     def parse_text(self, query_params: dict, **kwargs) -> str:
         return query_params.get("text")
