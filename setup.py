1e7410a59888f82fa617bfa8ccf39bab00eb1ef3
uezo <uezo@uezo.net>
2026-02-08 20:33:30 +0900

    Update for v0.2.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="speech_gateway",
-    version="0.2.2",
+    version="0.2.3",
     url="https://github.com/uezo/speech-gateway",
     author="uezo",
     author_email="uezo@uezo.net",
